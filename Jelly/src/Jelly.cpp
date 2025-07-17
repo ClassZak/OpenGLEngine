27b83e2ef577b7305cc5c484823786d010cdad77
Захар Голуб <pahargolub2@gmail.com>
2025-07-17 14:37:02 +0300

    Удаление всего лишнего

--- Jelly/src/Jelly.cpp
+++ Jelly/src/Jelly.cpp
@@ -20,8 +20,6 @@
 #include <thread>
 #include <utility>
 
-
-
 #include <math.h>
 
 
@@ -41,65 +39,24 @@
 #define ANIMATION_SPEED 2.5e-3
 
 
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="T">скалярный тип</typeparam>
-/// <param name="count"></param>
-/// <param name="center"></param>
-/// <returns></returns>
-template<typename T>
-static inline std::vector<Vertex2D<T>> GenerateCircleVertexes
-(std::size_t count, T radius, const Vertex2D<T>& center)
-{
-	std::vector<Vertex2D<T>> vertexes;
-	vertexes.reserve(count+1);
-
-	vertexes.emplace_back(center);
-
-	double sector = 2 * M_PI / count;
-
-	for (std::size_t i = 0; i < count; ++i)
-	{
-		T x = static_cast<T>(cos(sector * i)*radius), y = static_cast<T>(sin(sector * i)* radius);
-		vertexes.emplace_back(x + center.x, y + center.y);
-	}
-
-	vertexes.emplace_back(*std::next(vertexes.begin()));
-
-	return vertexes;
-}
-
-
-
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "Russian");
 
 	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
-
 	std::string shaderData = LoadDataFromFile("../res/shaders/shader.shader");
 	Shader shader = Shader::GetVertexAndFragmentShaders(shaderData);
 
-
-
 	GLFWwindow* window;
-
-
 
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
 
-
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-
-
 
 	window = glfwCreateWindow(640, 480, "Jelly", NULL, NULL);
 	if (!window)
@@ -126,15 +83,6 @@ int main(int argc, char** argv)
 		return -1;
 
 
-
-
-	unsigned int VAO1;
-	GLLogCall(glGenVertexArrays(1, &VAO1));
-	GLLogCall(glBindVertexArray(VAO1));
-
-
-
-
 	const std::vector<Vertex2D<float>> vertexes =
 	{
 		Vertex2D(-0.5f		,	-0.5f),
@@ -152,8 +100,9 @@ int main(int argc, char** argv)
 		0,	2,	3
 	};
 
-
-
+	unsigned int VAO1;
+	GLLogCall(glGenVertexArrays(1, &VAO1));
+	GLLogCall(glBindVertexArray(VAO1));
 
 	unsigned int VBO1{};
 	glGenBuffers(1, &VBO1);
@@ -180,79 +129,22 @@ int main(int argc, char** argv)
 		GL_STATIC_DRAW
 	);
 
-
-
-
-
-	unsigned int VAO2;
-	GLLogCall(glGenVertexArrays(1, &VAO2));
-	GLLogCall(glBindVertexArray(VAO2));
-
-	const unsigned int circlePointCount = 10;
-	float radius = 0.1f;
-	const Vertex2D<float> center(0.75f, 0.f);
-	std::vector<Vertex2D<float>> vertexes2 = GenerateCircleVertexes(circlePointCount, radius, center);
-	std::vector<Vertex2D<float>> vertexBufferData2 = vertexes2;
-	std::vector<unsigned int> circleVertexesIndices = 
-	Circle<float>::GenerateCircleVertexIndexes(circlePointCount);
-
-
-	// Генерация индексов для треугольников
-	std::vector<unsigned int> vertexesIndices2;
-	for (int i = 0; i < circlePointCount - 1; i++)
-	{
-		vertexesIndices2.push_back(0); // Центральная вершина
-		vertexesIndices2.push_back(1 + i); // Текущая вершина окружности
-		vertexesIndices2.push_back(1 + (i + 1) % circlePointCount); // Следующая вершина окружности
-	}
-
-
-	for(auto& el : vertexesIndices2)
-		std::cout<<el<<"\n";
-	std::cout<< vertexesIndices2.size()<<std::endl<<std::endl;
-	for(auto& el : circleVertexesIndices)
-		std::cout<<el<<"\n";
-	std::cout<< circleVertexesIndices.size()<<std::endl<<std::endl;
-
-	unsigned int VBO2;
-	glGenBuffers(1, &VBO2);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-	glBufferData
-	(
-		GL_ARRAY_BUFFER,
-		vertexBufferData2.size() * sizeof(Vertex2D<float>),
-		vertexBufferData2.data(),
-		GL_STATIC_DRAW
-	);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer
-	(0, VERTEX_ATTRIBUTE_SIZE, GL_FLOAT, GL_FALSE, sizeof(float) * VERTEX_ATTRIBUTE_SIZE, 0);
-
-	unsigned int IBO2;
-	glGenBuffers(1, &IBO2);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO2);
-	glBufferData
-	(
-		GL_ELEMENT_ARRAY_BUFFER,
-		vertexesIndices2.size() * sizeof(unsigned int),
-		vertexesIndices2.data(),
-		GL_STATIC_DRAW
-	);
-
-
-
-
-
 	GLuint shaderProgram = Shader::CreateShader(shader.m_vertexShader, shader.m_fragmentShader);
 	glUseProgram(shaderProgram);
 
 
 	int location = glGetUniformLocation(shaderProgram, "u_Color");
 	GL_ASSERT(location != -1);
-	GLLogCall(glUniform4f(location, 0.2f, 0.2f, 02.f, 1.f));
 
 
-	Circle circle(30,0.1f,Vertex2D<float>(0.75f,-0.5f));
+
+	Circle circle
+	(
+		30,
+		0.1f,
+		Vertex2D<float>(0.75f,-0.5f),
+		[&location](){glUniform4f(location, 0.2f, 0.2f, 02.f, 1.f);}
+	);
 	circle.Bind();
 
 
@@ -278,21 +170,6 @@ int main(int argc, char** argv)
 		vertexes[1].y + (sin(millisecondsSinceEpoch.count() * ANIMATION_SPEED) + 1) / 2.
 		- 0.5;
 
-
-
-		double sector = 
-		(2 * M_PI) * 
-		fmod((double)millisecondsSinceEpoch.count(),10000) / 10000 /
-		(circlePointCount-1)
-		;
-
-		for (std::size_t i = 1; i < vertexBufferData2.size(); ++i)
-		{
-			vertexBufferData2[i].x = 
-			center.x + cos(sector * (i-1)) * radius;
-			vertexBufferData2[i].y = 
-			center.y + sin(sector * (i-1)) * radius;
-		}
 
 		long diff = 2L;
 #if FPS <= 1000
@@ -340,32 +217,6 @@ int main(int argc, char** argv)
 		glDrawElements(GL_TRIANGLES, vertexesIndices.size(), GL_UNSIGNED_INT, nullptr);
 
 
-
-
-
-
-		glBindVertexArray(VAO2);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-		glBufferData
-		(
-			GL_ARRAY_BUFFER,
-			vertexBufferData2.size() * sizeof(std::vector<Vertex2D<float>>::value_type),
-			vertexBufferData2.data(),
-			GL_STATIC_DRAW
-		);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO2);
-		glUniform4f(location, 1.0f, 0.5f, 1.0f, 1.0f);
-		glBufferData
-		(
-			GL_ELEMENT_ARRAY_BUFFER,
-			vertexesIndices2.size() * sizeof(std::vector<unsigned int>::value_type) * 
-			vertexesIndices2.size(),
-			vertexesIndices2.data(),
-			GL_STATIC_DRAW
-		);
-		glDrawElements(GL_TRIANGLES, vertexesIndices2.size(), GL_UNSIGNED_INT, nullptr);
-
-
 		circle.Draw();
 
 
@@ -381,7 +232,6 @@ int main(int argc, char** argv)
 	glDeleteProgram(shaderProgram);
 
 	glDeleteVertexArrays(1, &VAO1);
-	glDeleteVertexArrays(1, &VAO2);
 
 	glfwTerminate();
 
