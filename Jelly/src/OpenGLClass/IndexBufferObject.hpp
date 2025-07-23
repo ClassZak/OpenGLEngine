c2584afb03e73eefc3f2ea3b5a85a0604794f74b
Захар Голуб <pahargolub2@gmail.com>
2025-07-23 16:38:47 +0300

    Обновление OpenGL классов

--- Jelly/src/OpenGLClass/IndexBufferObject.hpp
+++ Jelly/src/OpenGLClass/IndexBufferObject.hpp
@@ -1,6 +1,62 @@
 #pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+#include <map>
+#include <functional>
+
+#include "../utils/GLMacro.h"
+
+class IndexBufferObject
+{
+private:
+	// Renderer id
+	GLuint m_index = 0;
+	GLsizeiptr count = 0;
+
+	IndexBufferObject(const std::vector<GLuint>& dataVector)
+		: IndexBufferObject(dataVector.data(), dataVector.size() * sizeof(GLuint))
+	{
+	}
+	IndexBufferObject(const GLuint* data, GLsizeiptr size)
+	{
+		Init(data, size);
+	}
+
+	~IndexBufferObject()
+	{
+		glDeleteBuffers(1, &m_index);
+	}
+public:
+	void Init(const std::vector<GLuint>& dataVector)
+	{
+		Init(dataVector.data(), dataVector.size());
+	}
+
+	void Init(const GLuint* data, GLsizeiptr count)
+	{
+		GLLogCall(glGenBuffers(1, &this->m_index));
+		GLLogCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->m_index));
+		glBufferData
+		(
+			GL_ELEMENT_ARRAY_BUFFER,
+			count * sizeof(GLuint),
+			data,
+			GL_STATIC_DRAW
+		);
+	}
 
 
+	void Bind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->m_index);
+	}
+
+	void UnBind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+};
 
 
 
--- Jelly/src/OpenGLClass/VertexBufferObject.hpp
+++ Jelly/src/OpenGLClass/VertexBufferObject.hpp
@@ -1,25 +1,60 @@
 #pragma once
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <map>
+#include <functional>
+
+#include "../utils/GLMacro.h"
 
 class VertexBufferObject
 {
 private:
+	// Renderer id
 	GLuint m_index=0;
 
-public:
-	void Bind()
+
+	template<class T>
+	VertexBufferObject(const std::vector<T>& dataVector)
+	: VertexBufferObject(dataVector.data(), dataVector.size() * sizeof(T))
+	{ }
+	VertexBufferObject(const void* data, GLsizeiptr size)
 	{
-		
+		Init(data, size);
 	}
-	void UnBind()
+
+	~VertexBufferObject()
 	{
-		if (m_index != GL_FALSE)
-		{
-			glDeleteBuffers(1, &m_index);
-			m_index = GL_FALSE;
-		}
+		glDeleteBuffers(1, &m_index);
+	}
+public:
+	template<class T>
+	void Init(const std::vector<T>& dataVector)
+	{
+		Init(dataVector.data(), dataVector.size() * sizeof(T));
+	}
+	void Init(const void* data, GLsizeiptr size)
+	{
+		GLLogCall(glGenBuffers(1, &this->m_index));
+		GLLogCall(glBindBuffer(GL_ARRAY_BUFFER, this->m_index));
+		glBufferData
+		(
+			GL_ARRAY_BUFFER,
+			size,
+			data,
+			GL_STATIC_DRAW
+		);
+	}
+
+
+	void Bind() const
+	{
+		GLLogCall(glBindBuffer(GL_ARRAY_BUFFER, this->m_index));
+	}
+	
+	void UnBind() const
+	{
+		GLLogCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
 	}
 };
 
